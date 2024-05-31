e02f688dfa9eb431e506af72dbe6946e7b410327
varun <varunvnairktda@gmail.com>
2024-05-31 17:18:11 +0530

    added signup and forgot password page

--- sign/urls.py
+++ sign/urls.py
@@ -3,5 +3,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.login,name='login')
+    path('',views.login,name='login'),
+    path('forgot_password/',views.forgot_password,name='forgot_password'),
+    path('sign_up/',views.sign_up,name='sign_up'),
 ]
--- sign/views.py
+++ sign/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def login(request):
     return render(request,'index.html')
+def forgot_password(request):
+    return render(request,'forgot_password.html')
+def sign_up(request):
+    return render(request,'sign_up.html')
