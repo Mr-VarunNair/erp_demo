b3ed644088edeeb001c478aef0e479c21a602244
varun <varunvnairktda@gmail.com>
2024-06-06 11:42:11 +0530

    added all the href link to nav bar

--- sign/urls.py
+++ sign/urls.py
@@ -3,7 +3,21 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.login,name='login'),
-    path('forgot_password/',views.forgot_password,name='forgot_password'),
-    path('sign_up/',views.sign_up,name='sign_up'),
+    path('',views.home,name='home'),
+    path('property_management/',views.prpty_mng,name='property_management'),
+    path('amc_mng/',views.amc_mng,name='amc_mng'),
+    path('store_mng/',views.store_mng,name='store_mng'),
+    path('rem_schedule/',views.rem_schedule,name='rem_schedule'),
+    path('vehicle_mng/',views.vehicle_mng,name='vehicle_mng'),
+    path('asset_mng/',views.asset_mng,name='asset_mng'),
+    path('build_mnt/',views.build_mnt,name='build_mnt'),
+    path('iso_mng/',views.iso_mng,name='iso_mng'),
+    path('house_mng/',views.house_mng,name='house_mng'),
+    path('purchase_mng/',views.purchase_mng,name='purchase_mng'),
+    path('tax_building/',views.tax_building,name='tax_building'),
+    path('tenant_mng/',views.tenant_mng,name='tenant_mng')
+
+    #path('login',views.login,name='login'),
+    #path('forgot_password/',views.forgot_password,name='forgot_password'),
+    #path('sign_up/',views.sign_up,name='sign_up'),
 ]
--- sign/views.py
+++ sign/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
-def login(request):
-    return render(request,'index.html')
-def forgot_password(request):
-    return render(request,'forgot_password.html')
-def sign_up(request):
-    return render(request,'sign_up.html')
+def home(request):
+    return render(request,'home.html')
+def prpty_mng(request):
+    return render(request,'prpty_mng.html')
+def amc_mng(request):
+    return render(request,'amc_mng.html')
+def store_mng(request):
+    return render(request,'store_mng.html')
+def rem_schedule(request):
+    return render(request,'rem_schedule.html')
+def vehicle_mng(request):
+    return render(request,'vehicle_mng.html')
+def asset_mng(request):
+    return render(request,'asset_mng.html')
+def build_mnt(request):
+    return render(request,'build_mnt.html')
+def iso_mng(request):
+    return render(request,'iso_mng.html')
+def house_mng(request):
+    return render(request,'house_mng.html')
+def purchase_mng(request):
+    return render(request,'purchase_mng.html')
+def tax_building(request):
+    return render(request,'tax_building.html')
+def tenant_mng(request):
+    return render(request,'tenant_mng.html')
